f0b8187132bd812296e9b69d75b8cc9f1394146f
Jud <Jud.Stephenson@gmail.com>
2013-07-17 17:41:10 -0400

    Update python-slowaes.mk

--- python-slowaes.mk
+++ python-slowaes.mk
@@ -7,7 +7,7 @@
 PYTHON_SLOWAES_VERSION = 0.1a1
 PYTHON_SLOWAES_SITE = https://pypi.python.org/packages/source/s/slowaes
 PYTHON_SLOWAES_SOURCE = slowaes-$(PYTHON_SLOWAES_VERSION).tar.gz
-PYTHON_SLOWAES_DEPENDENCIES = python python-setuptools
+PYTHON_SLOWAES_DEPENDENCIES = python host-python-setuptools
 
 define PYTHON_SLOWAES_BUILD_CMDS
 	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build)
